1da4ed739444863bcf207b9914e189106ecae2c8
ZhenlyChen <zhenlychen@foxmail.com>
2017-07-04 23:56:25 +0800

    完成前端部分页面

--- service/app.js
+++ service/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const server = app.listen(30001, '127.0.0.1', function() {
+const server = app.listen(30020, '127.0.0.1', function() {
   var host = server.address().address;
   var port = server.address().port;
   console.log('App listening at http://%s:%s', host, port);
@@ -18,3 +18,18 @@ app.post('/post', function(req, res) {
   console.log(req.body);
   res.send({ state: 'ok' });
 });
+
+// todo
+/*
+  1. 注册
+  2. 登陆
+      2.1 直接post登陆
+      2.2 网站服务器post登陆
+      2.3 授权码登陆
+  3. 邮箱认证get
+  4. 发送邮件验证码
+  5. 发送激活邮件
+  6. 重置密码
+  7. 授权码验证
+  8. 邮件系统
+*/
